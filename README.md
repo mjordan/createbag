3a672ba5d8b40c059ec082b86c0b57f11080234a
Mark Jordan <mjordan@sfu.ca>
2014-10-11 09:51:21 -0700

    Updated README, added script and config file.

--- README.md
+++ README.md
@@ -1,4 +1,27 @@
-createbag
-=========
+# Create Bag
 
 A simple utility to allow users to create a Bag from a directory on their file system.
+
+## Features so far
+
+* Uses standard file/directory picker to allow user to select which directory to create Bag from.
+* BagIt tags can be defined in a config file.
+
+## Python dependencies
+
+* [Python GTK+3](http://python-gtk-3-tutorial.readthedocs.org/en/latest/index.html)
+* [bagit](https://github.com/LibraryOfCongress/bagit-python)
+
+## Usage
+
+Until Create Bag is compiled into native binaries, invoke it from the command line like this:
+
+`python createbag.py`
+
+and click on "Choose a directory to create Bag from" and go. Note that the Bag is created in place, meaning that the directory is replaced with the Bagged version.
+
+
+## To do
+
+* Port to Windows and OS X native binaries
+* Add error handling, etc.
---
+++ createbag.py
@@ -0,0 +1,66 @@
+"""
+Simple tool to create a Bag from a filesystem folder.
+
+@todo:
+  - Add error handling around the call to bagit.
+  - Get working on Windows.
+"""
+
+import ConfigParser
+import bagit
+from gi.repository import Gtk
+
+config = ConfigParser.ConfigParser()
+config.optionxform = str
+config.read('config.cfg')
+
+# Get tags from config file.
+bagit_tags = {}
+tags = config.options('Tags')
+for tag in tags:
+    bagit_tags[tag] = config.get('Tags', tag)
+
+class FolderChooserWindow(Gtk.Window):
+
+    def __init__(self):
+        Gtk.Window.__init__(self, title="Create a Bag")
+        self.set_border_width(10)
+        self.move(200, 200)
+
+        box = Gtk.Box(spacing=6)
+        self.add(box)
+       
+        choose_folder_button = Gtk.Button("Choose a folder to create Bag from")
+        choose_folder_button.connect("clicked", self.on_folder_clicked)
+        box.add(choose_folder_button)
+
+        quit_button = Gtk.Button("Quit")
+        quit_button.connect("clicked", Gtk.main_quit)
+        box.add(quit_button)
+
+    def on_folder_clicked(self, widget):
+        folder_picker_dialog = Gtk.FileChooserDialog("Please choose a folder", self,
+            Gtk.FileChooserAction.SELECT_FOLDER,
+            (Gtk.STOCK_CANCEL, Gtk.ResponseType.CANCEL,
+            "Select", Gtk.ResponseType.OK))
+        folder_picker_dialog.set_default_size(800, 400)
+
+        response = folder_picker_dialog.run()
+        if response == Gtk.ResponseType.OK:
+            bag = bagit.make_bag(folder_picker_dialog.get_filename(), bagit_tags)
+            confirmation_dialog = Gtk.MessageDialog(self, 0, Gtk.MessageType.INFO,
+            Gtk.ButtonsType.OK, "Bag created")
+            confirmation_dialog.format_secondary_text(
+                "The Bag for folder %s has been created." % folder_picker_dialog.get_filename())
+            confirmation_dialog.run()
+            confirmation_dialog.destroy()
+        elif response == Gtk.ResponseType.CANCEL:
+            print("Operation cancelled")
+
+        folder_picker_dialog.destroy()
+
+
+win = FolderChooserWindow()
+win.connect("delete-event", Gtk.main_quit)
+win.show_all()
+Gtk.main()
